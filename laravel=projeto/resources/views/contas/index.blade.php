0244c543963d3109e4d904bf6e07cb50fa712cae
Joao Santos <jmsdo1970@gmail.com>
2024-07-01 20:54:54 -0300

    first commit

---
+++ laravel=projeto/resources/views/contas/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="{{route('conta.create')}}">Cadastrar</a><br>
+
+    <h1>Listar as contas</h1>
+    
+    <a href="{{route('conta.show')}}">Visualizar</a><br>
+    <a href="{{route('conta.edit')}}">Editar</a><br>
+    <!-- <a href="{{route('conta.destroy')}}">Apagar</a><br> -->
+</body>
+</html>
---
+++ laravel=projeto/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index-conta', [ContaController::class, 'index'])->name('conta.index');
+Route::get('/create-conta', [ContaController::class, 'create'])->name('conta.create');
+Route::post('/store-conta', [ContaController::class, 'store'])->name('conta.store');
+Route::get('/show-conta', [ContaController::class, 'show'])->name('conta.show');
+Route::get('/edit-conta', [ContaController::class, 'edit'])->name('conta.edit');
+Route::put('/update-conta', [ContaController::class, 'update'])->name('conta.update');
+Route::delete('/destroy-conta', [ContaController::class, 'destroy'])->name('conta.destroy');
